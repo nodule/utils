7d56725304c41dd9db955b04c4b7966586e0b1eb
Rob Halff <rob.halff@gmail.com>
2013-12-21 20:44:21 +0100

    update

---
+++ nodes/spawn/node.js
@@ -0,0 +1,16 @@
+var cmd = child_process.spawn('ls', ['-lh', '/usr']);
+
+ls.stdout.on('data', function (data) {
+  console.log('stdout: ' + data);
+});
+
+ls.stderr.on('data', function (data) {
+  console.log('stderr: ' + data);
+});
+
+ls.on('close', function (code) {
+  console.log('child process exited with code ' + code);
+});
+
+
+output = [slow, 'down', input.timeout]
