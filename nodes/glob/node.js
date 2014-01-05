981236595e02ea04e6ad7212114e00e24eaf1857
Rob Halff <rob.halff@gmail.com>
2014-01-05 02:40:26 +0100

    re-arrange glob

--- nodes/glob/node.js
+++ nodes/glob/node.js
@@ -1,4 +1,35 @@
-var obj = {};
-obj.glob = glob;
+output = function(cb) {
 
-output = [obj, 'glob', input.match];
+  var mg = new glob.Glob(input.match, {}, function(err, matches) {
+
+    cb({
+      matches: matches
+    });
+  
+  });
+
+  mg.on('match', function(match) {
+
+    cb({
+      match: match
+    });
+
+  });
+
+  mg.on('error', function(err) {
+
+    cb({
+      error: err 
+    });
+
+  });
+
+  mg.on('abort', function() {
+
+    cb({
+      abort: null 
+    });
+
+  });
+
+}
