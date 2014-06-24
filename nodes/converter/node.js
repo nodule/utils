0670509d22c33db85fad668767ecfdf143612d64
Rob Halff <rob.halff@gmail.com>
2014-06-25 00:52:25 +0200

    Buffer

--- nodes/converter/node.js
+++ nodes/converter/node.js
@@ -1,4 +1,4 @@
 on.input.in = function() {
-  var d = new buffer_browserify(data, input.from);
+  var d = new buffer_browserify.Buffer(data, input.from);
   output( { out: d.toString(input.to) });
 };
