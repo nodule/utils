6fd0c4b1a9bea84859a98ae83a5b116fc15aba3a
Rob Halff <rob.halff@gmail.com>
2014-06-29 08:54:08 +0200

    fix \n bug

--- nodes/converter/node.js
+++ nodes/converter/node.js
@@ -1,4 +1,9 @@
 on.input.in = function() {
+
+  // \n replace shouldn't be necessary.
+  if(typeof data === 'string') {
+    data = data.replace('\n', '');
+  }
   var d = new buffer.Buffer(data, input.from);
   output( { out: d.toString(input.to) });
 };
